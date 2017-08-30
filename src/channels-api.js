db55500b13470792f8e696d6229cf703a2ef12e7
mick <michael@maithu.com>
2017-08-30 12:58:25 +0100

    change json key values to match channels_api

--- src/channels-api.js
+++ src/channels-api.js
@@ -386,23 +386,23 @@ class ChannelsApi implements IStreamingAPI {
     }
   }
 
-  request(stream: string, payload: Object, requestId: string=UUID.generate()): Promise<Object> {
+  request(stream: string, payload: Object, request_id: string=UUID.generate()): Promise<Object> {
     return new Promise((resolve, reject) => {
-      const selector = this._buildRequestResponseSelector(stream, requestId);
+      const selector = this._buildRequestResponseSelector(stream, request_id);
 
       this.dispatcher.once(selector, data => {
         const {payload: response} = data;
-        const {responseStatus} = response;
+        const {response_status} = response;
 
         // 2xx is success
-        if (Math.floor(responseStatus / 100) === 2) {
+        if (Math.floor(response_status / 100) === 2) {
           resolve(response.data);
         } else {
           reject(response);
         }
       });
 
-      const message = this._buildMultiplexedMessage(stream, Object.assign({}, payload, {requestId}));
+      const message = this._buildMultiplexedMessage(stream, Object.assign({}, payload, {request_id}));
       this.send(message);
     });
   }
@@ -457,10 +457,10 @@ class ChannelsApi implements IStreamingAPI {
     return {stream, payload};
   }
 
-  _buildRequestResponseSelector(stream: string, requestId: string) {
+  _buildRequestResponseSelector(stream: string, request_id: string) {
     return {
       stream,
-      payload: {requestId},
+      payload: {request_id},
     };
   }
 
